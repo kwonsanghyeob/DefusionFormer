e5b7b44e9277a1cc9195d2e0f687ec3ee3e42e96
sanghyeob <sueb4509@hanyang.ac.kr>
2024-04-05 14:45:09 +0900

    label length 결과 추가

--- main_hyper_label_plot.py
+++ main_hyper_label_plot.py
@@ -21,25 +21,27 @@ if __name__ == '__main__':
     plt.rcParams['font.family'] = 'Times New Roman'
     plt.rc('font', size=25)
 
-    plt.figure(num = 1, figsize=(12,8))
-    plt.subplot(2,1,1)
-    plt.title('MSE')
+    plt.figure(num = 1, figsize=(10,6))
+    # plt.subplot(2,1,1)
     plt.plot(LL,result[:,0], linestyle='dashed',marker='o', color = 'red', linewidth = 3)
     # plt.axvspan(65,67,color = 'grey', alpha = 1)
-    plt.annotate('Optimal label length', xy=(67, min(result[:, 0])+0.0005), xytext =(70,min(result[:, 0])),arrowprops=dict(facecolor='green', shrink=0.001))
+    plt.annotate('Optimal label length', xy=(25, min(result[:, 0])), xytext =(35,min(result[:, 0])+0.005),arrowprops=dict(facecolor='green', shrink=0.001))
+    plt.ylabel('MSE')
     plt.xticks(LL)
-    plt.xlabel('label_length')
-    plt.grid()
-
-
-    plt.subplot(2,1, 2)
-    plt.title('MAE')
-    plt.plot(LL,result[:, 1], linestyle='dashed',marker='o', color = 'red', linewidth = 3)
-    # plt.axvspan(65, 67, color='grey', alpha=1)
-    plt.annotate('Optimal label length', xy=(67, min(result[:, 1])+0.001), xytext=(70, min(result[:, 1])), arrowprops=dict(facecolor='green', shrink=0.001))
-    plt.xlabel('Label length')
-    plt.xticks(LL)
+    plt.xlabel('label length')
     plt.grid()
     plt.tight_layout()
+    plt.show()
+
+    #
+    # plt.subplot(2,1, 2)
+    # plt.title('MAE')
+    # plt.plot(LL,result[:, 1], linestyle='dashed',marker='o', color = 'red', linewidth = 3)
+    # # plt.axvspan(65, 67, color='grey', alpha=1)
+    # plt.annotate('Optimal label length', xy=(25, min(result[:, 1])+0.001), xytext=(35, min(result[:, 1])), arrowprops=dict(facecolor='green', shrink=0.001))
+    # plt.xlabel('Label length')
+    # plt.xticks(LL)
+    # plt.grid()
+    # plt.tight_layout()
 
     plt.show()
